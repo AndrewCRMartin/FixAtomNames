1dbbcb618786ab9d73d440709a17da7e6f9fb966
Andrew Martin <Andrew C.R. Martin>
2023-03-10 14:08:16 +0000

    Added other amino acids

--- checkleu.c
+++ checkleu.c
@@ -61,7 +61,9 @@ void DoAnalysis(PDB *pdb)
    for(res=pdb; res!=NULL; res=nextres)
    {
       nextres = blFindNextResidue(res);
-      if(!strncmp(res->resnam, "LEU", 3))
+      if(!strncmp(res->resnam, "LEU", 3) ||
+         !strncmp(res->resnam, "PHE", 3) ||
+         !strncmp(res->resnam, "TYR", 3))
       {
          PDB *CA, *CB, *CG, *CD1, *CD2;
          REAL tor1, tor2, diff;
@@ -82,8 +84,58 @@ void DoAnalysis(PDB *pdb)
          if((diff < 90) || (diff > 180))
             strcpy(label, "SWAPPED!");
          
-         printf("LEU Tor1: %8.3f Tor2: %8.3f Diff: %8.3f %s\n",
-                tor1, tor2, diff, label);
+         printf("%s Tor1: %8.3f Tor2: %8.3f Diff: %8.3f %s\n",
+                res->resnam, tor1, tor2, diff, label);
+
+      }
+      else if(!strncmp(res->resnam, "ASP", 3))
+      {
+         PDB *CA, *CB, *CG, *OD1, *OD2;
+         REAL tor1, tor2, diff;
+         char label[16];
+            
+         CA  = blFindAtomInRes(res, "CA  ");
+         CB  = blFindAtomInRes(res, "CB  ");
+         CG  = blFindAtomInRes(res, "CG  ");
+         OD1 = blFindAtomInRes(res, "OD1 ");
+         OD2 = blFindAtomInRes(res, "OD2 ");
+         
+         tor1 = CalcTorsion(CA, CB, CG, OD1, FALSE);
+         tor2 = CalcTorsion(CA, CB, CG, OD2, FALSE);
+
+         diff = CalcDiff(tor1, tor2);
+         strcpy(label, "OK");
+         
+         if((diff < 90) || (diff > 180))
+            strcpy(label, "SWAPPED!");
+         
+         printf("%s Tor1: %8.3f Tor2: %8.3f Diff: %8.3f %s\n",
+                res->resnam, tor1, tor2, diff, label);
+
+      }
+      else if(!strncmp(res->resnam, "GLU", 3))
+      {
+         PDB  *CB, *CG, *CD, *OE1, *OE2;
+         REAL tor1, tor2, diff;
+         char label[16];
+            
+         CB  = blFindAtomInRes(res, "CB  ");
+         CG  = blFindAtomInRes(res, "CG  ");
+         CD  = blFindAtomInRes(res, "CD  ");
+         OE1 = blFindAtomInRes(res, "OE1 ");
+         OE2 = blFindAtomInRes(res, "OE2 ");
+         
+         tor1 = CalcTorsion(CB, CG, CD, OE1, FALSE);
+         tor2 = CalcTorsion(CB, CG, CD, OE2, FALSE);
+
+         diff = CalcDiff(tor1, tor2);
+         strcpy(label, "OK");
+         
+         if((diff < 90) || (diff > 180))
+            strcpy(label, "SWAPPED!");
+         
+         printf("%s Tor1: %8.3f Tor2: %8.3f Diff: %8.3f %s\n",
+                res->resnam, tor1, tor2, diff, label);
 
       }
       else if(!strncmp(res->resnam, "VAL", 3))
@@ -107,7 +159,7 @@ void DoAnalysis(PDB *pdb)
          if((diff < 90) || (diff > 180))
             strcpy(label, "SWAPPED!");
          
-         printf("VAL Tor1: %8.3f Tor2: %8.3f Diff: %8.3f %s\n",
+         printf("VAL  Tor1: %8.3f Tor2: %8.3f Diff: %8.3f %s\n",
                 tor1, tor2, diff, label);
 
       }
@@ -133,10 +185,42 @@ void DoAnalysis(PDB *pdb)
          if((diff < 90) || (diff > 180))
             strcpy(label, "SWAPPED!");
          
-         printf("ILE Tor1: %8.3f Tor2: %8.3f Diff: %8.3f %s\n",
+         printf("ILE  Tor1: %8.3f Tor2: %8.3f Diff: %8.3f %s\n",
                 tor1, tor2, diff, label);
 
       }
+      else if(!strncmp(res->resnam, "ARG", 3))
+      {
+         PDB  *CD, *NE, *CZ, *NH1, *NH2;
+         REAL tor1, tor2, diff;
+         char label[16];
+            
+         CD  = blFindAtomInRes(res, "CD  ");
+         NE  = blFindAtomInRes(res, "NE  ");
+         CZ  = blFindAtomInRes(res, "CZ  ");
+         NH1 = blFindAtomInRes(res, "NH1 ");
+         NH2 = blFindAtomInRes(res, "NH2 ");
+
+         tor1 = CalcTorsion(CD, NE, CZ, NH1, FALSE);
+         tor2 = CalcTorsion(CD, NE, CZ, NH2, FALSE);
+
+         diff = CalcDiff(tor1, tor2);
+         strcpy(label, "OK");
+         
+         if((diff < 90) || (diff > 180))
+            strcpy(label, "SWAPPED!");
+         
+         printf("ARG  Tor1: %8.3f Tor2: %8.3f Diff: %8.3f %s\n",
+                tor1, tor2, diff, label);
+
+      }
+
+
+
+
+
+
+
    }
 }
 
@@ -154,13 +238,31 @@ void FixLeu(PDB *pdb)
          atom[i] = NULL;
             
       nextres = blFindNextResidue(res);
-      if(!strncmp(res->resnam, "LEU", 3))
+      if(!strncmp(res->resnam, "LEU", 3) ||
+         !strncmp(res->resnam, "PHE", 3) ||
+         !strncmp(res->resnam, "TYR", 3))
       {
          atom[0] = blFindAtomInRes(res, "CA  ");
          atom[1] = blFindAtomInRes(res, "CB  ");
          atom[2] = blFindAtomInRes(res, "CG  ");
          atom[3] = blFindAtomInRes(res, "CD1 ");
          atom[4] = blFindAtomInRes(res, "CD2 ");
+      }
+      else if(!strncmp(res->resnam, "ASP", 3))
+      {
+         atom[0] = blFindAtomInRes(res, "CA  ");
+         atom[1] = blFindAtomInRes(res, "CB  ");
+         atom[2] = blFindAtomInRes(res, "CG  ");
+         atom[3] = blFindAtomInRes(res, "OD1 ");
+         atom[4] = blFindAtomInRes(res, "OD2 ");
+      }
+      else if(!strncmp(res->resnam, "GLU", 3))
+      {
+         atom[0] = blFindAtomInRes(res, "CB  ");
+         atom[1] = blFindAtomInRes(res, "CG  ");
+         atom[2] = blFindAtomInRes(res, "CD  ");
+         atom[3] = blFindAtomInRes(res, "OE1 ");
+         atom[4] = blFindAtomInRes(res, "OE2 ");
       }
       else if(!strncmp(res->resnam, "VAL", 3))
       {
@@ -179,6 +281,14 @@ void FixLeu(PDB *pdb)
          atom[3] = blFindAtomInRes(res, "CG2 ");
          atom[4] = blFindAtomInRes(res, "CG1 ");
       }
+      else if(!strncmp(res->resnam, "ARG", 3))
+      {
+         atom[0] = blFindAtomInRes(res, "CD  ");
+         atom[1] = blFindAtomInRes(res, "NE  ");
+         atom[2] = blFindAtomInRes(res, "CZ  ");
+         atom[3] = blFindAtomInRes(res, "NH1 ");
+         atom[4] = blFindAtomInRes(res, "NH2 ");
+      }
       
          
       if(atom[0] != NULL)
@@ -193,9 +303,17 @@ void FixLeu(PDB *pdb)
             if((diff < 90) || (diff > 180))
             {
                SwapAtomCoords(atom[3], atom[4]);
+               
+               if(!strncmp(res->resnam, "PHE", 3) ||
+                  !strncmp(res->resnam, "TYR", 3))
+               {
+                  atom[5] = blFindAtomInRes(res, "CE1 ");
+                  atom[6] = blFindAtomInRes(res, "CE2 ");
+
+                  SwapAtomCoords(atom[5], atom[6]);
+               }
             }
          }
-         
       }
    }
 }
@@ -204,26 +322,16 @@ void FixLeu(PDB *pdb)
 void SwapAtomCoords(PDB *atom1, PDB *atom2)
 {
    PDB tmp;
-   REAL x,y,z;
-   
-   fprintf(stderr,"Before: atom1 %.3f %.3f %.3f\n", atom1->x, atom1->y, atom1->z);
-   fprintf(stderr,"        atom2 %.3f %.3f %.3f\n", atom2->x, atom2->y, atom2->z);
-   
-   
-   x = atom1->x;
-   y = atom1->y;
-   z = atom1->z;
+
+   tmp = *atom1;
    
    atom1->x = atom2->x;
    atom1->y = atom2->y;
    atom1->z = atom2->z;
-   
-   atom2->x = x;
-   atom2->y = y;
-   atom2->x = z;
 
-   fprintf(stderr,"After:  atom1 %.3f %.3f %.3f\n",   atom1->x, atom1->y, atom1->z);
-   fprintf(stderr,"        atom2 %.3f %.3f %.3f\n\n", atom2->x, atom2->y, atom2->z);
+   atom2->x = tmp.x;
+   atom2->y = tmp.y;
+   atom2->z = tmp.z;
 }
 
 
